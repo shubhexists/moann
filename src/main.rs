12600c1a53d96717a1b926cff119e8205bd679f1
Shubham Singh <shubh622005@gmail.com>
2024-11-15 01:30:02 +0530

    feat: map for all keys

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,154 @@
 use console::Term;
+use rdev::{listen, Event, EventType};
 use rodio::{source::Source, Decoder, OutputStream};
 use std::fs::File;
 use std::io::BufReader;
 
 fn main() {
-    let stdout = Term::buffered_stdout();
+    let _ = Term::buffered_stdout();
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
 
-    'game_loop: loop {
-        let file = BufReader::new(
-            File::open("/home/jerry/Desktop/projects/sound/mech-keyboard-02-102918.mp3").unwrap(),
-        );
-        let source = Decoder::new(file).unwrap();
-        if let Ok(character) = stdout.read_char() {
-            match character {
-                'w' => {
-                    stream_handle
+    listen(move |event: Event| {
+        if let EventType::KeyPress(key) = event.event_type {
+            let file = BufReader::new(
+                File::open("/home/jerry/Desktop/projects/sound/mech-keyboard-02-102918.mp3")
+                    .unwrap(),
+            );
+            let source = Decoder::new(file).unwrap();
+            match key {
+                rdev::Key::KeyA => {
+                    let _ = stream_handle
                         .play_raw(source.convert_samples())
                         .map_err(|_| panic!(" I dont know the error"));
                 }
-                'a' => {
-                    stream_handle
+                rdev::Key::KeyB => {
+                    let _ = stream_handle
                         .play_raw(source.convert_samples())
                         .map_err(|_| panic!(" I dont know the error"));
                 }
-                's' => {
-                    stream_handle
+                rdev::Key::KeyC => {
+                    let _ = stream_handle
                         .play_raw(source.convert_samples())
                         .map_err(|_| panic!(" I dont know the error"));
                 }
-                'd' => {
-                    stream_handle
+                rdev::Key::KeyD => {
+                    let _ = stream_handle
                         .play_raw(source.convert_samples())
                         .map_err(|_| panic!(" I dont know the error"));
                 }
-                _ => break 'game_loop,
+                rdev::Key::KeyE => {
+                    let _ = stream_handle
+                        .play_raw(source.convert_samples())
+                        .map_err(|_| panic!(" I dont know the error"));
+                }
+                rdev::Key::KeyF => {
+                    let _ = stream_handle
+                        .play_raw(source.convert_samples())
+                        .map_err(|_| panic!(" I dont know the error"));
+                }
+                rdev::Key::KeyG => {
+                    let _ = stream_handle
+                        .play_raw(source.convert_samples())
+                        .map_err(|_| panic!(" I dont know the error"));
+                }
+                rdev::Key::KeyH => {
+                    let _ = stream_handle
+                        .play_raw(source.convert_samples())
+                        .map_err(|_| panic!(" I dont know the error"));
+                }
+                rdev::Key::KeyI => {
+                    let _ = stream_handle
+                        .play_raw(source.convert_samples())
+                        .map_err(|_| panic!(" I dont know the error"));
+                }
+                rdev::Key::KeyJ => {
+                    let _ = stream_handle
+                        .play_raw(source.convert_samples())
+                        .map_err(|_| panic!(" I dont know the error"));
+                }
+                rdev::Key::KeyK => {
+                    let _ = stream_handle
+                        .play_raw(source.convert_samples())
+                        .map_err(|_| panic!(" I dont know the error"));
+                }
+                rdev::Key::KeyL => {
+                    let _ = stream_handle
+                        .play_raw(source.convert_samples())
+                        .map_err(|_| panic!(" I dont know the error"));
+                }
+                rdev::Key::KeyM => {
+                    let _ = stream_handle
+                        .play_raw(source.convert_samples())
+                        .map_err(|_| panic!(" I dont know the error"));
+                }
+                rdev::Key::KeyN => {
+                    let _ = stream_handle
+                        .play_raw(source.convert_samples())
+                        .map_err(|_| panic!(" I dont know the error"));
+                }
+                rdev::Key::KeyO => {
+                    let _ = stream_handle
+                        .play_raw(source.convert_samples())
+                        .map_err(|_| panic!(" I dont know the error"));
+                }
+                rdev::Key::KeyP => {
+                    let _ = stream_handle
+                        .play_raw(source.convert_samples())
+                        .map_err(|_| panic!(" I dont know the error"));
+                }
+                rdev::Key::KeyQ => {
+                    let _ = stream_handle
+                        .play_raw(source.convert_samples())
+                        .map_err(|_| panic!(" I dont know the error"));
+                }
+                rdev::Key::KeyR => {
+                    let _ = stream_handle
+                        .play_raw(source.convert_samples())
+                        .map_err(|_| panic!(" I dont know the error"));
+                }
+                rdev::Key::KeyS => {
+                    let _ = stream_handle
+                        .play_raw(source.convert_samples())
+                        .map_err(|_| panic!(" I dont know the error"));
+                }
+                rdev::Key::KeyT => {
+                    let _ = stream_handle
+                        .play_raw(source.convert_samples())
+                        .map_err(|_| panic!(" I dont know the error"));
+                }
+                rdev::Key::KeyU => {
+                    let _ = stream_handle
+                        .play_raw(source.convert_samples())
+                        .map_err(|_| panic!(" I dont know the error"));
+                }
+                rdev::Key::KeyV => {
+                    let _ = stream_handle
+                        .play_raw(source.convert_samples())
+                        .map_err(|_| panic!(" I dont know the error"));
+                }
+                rdev::Key::KeyW => {
+                    let _ = stream_handle
+                        .play_raw(source.convert_samples())
+                        .map_err(|_| panic!(" I dont know the error"));
+                }
+                rdev::Key::KeyX => {
+                    let _ = stream_handle
+                        .play_raw(source.convert_samples())
+                        .map_err(|_| panic!(" I dont know the error"));
+                }
+                rdev::Key::KeyY => {
+                    let _ = stream_handle
+                        .play_raw(source.convert_samples())
+                        .map_err(|_| panic!(" I dont know the error"));
+                }
+                rdev::Key::KeyZ => {
+                    let _ = stream_handle
+                        .play_raw(source.convert_samples())
+                        .map_err(|_| panic!(" I dont know the error"));
+                }
+                _ => {}
             }
         }
-    }
+    })
+    .expect("Failed to start global key listener");
 }
